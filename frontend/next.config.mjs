4171ca92687c8f546dfa484e99f1d6e06740512a
Brian Quinney <quinneybrianm@gmail.com>
2024-07-26 16:58:40 -0400

    trying to fix netlify deploy errors

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build'
+    distDir: 'build',
+    output: 'export',
+    trailingSlash: true,
 };
 
 export default nextConfig;
