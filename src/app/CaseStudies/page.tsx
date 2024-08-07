29add7fd7f7b9330a017e95da15cc79c7c522487
Brian Quinney <quinneybrianm@gmail.com>
2024-08-07 12:18:33 -0400

    fixing for heroku

--- src/app/CaseStudies/page.tsx
+++ src/app/CaseStudies/page.tsx
@@ -1,111 +1,109 @@
-// "use client"
+"use client"
 
-// import React, { useEffect } from "react"
-// import Image from "next/image"
-// import './page.scss'
-// import { hd1080Min, hd4kMin, mobileMin, tabletMin } from "../_vars"
+import React from "react"
+import './page.scss'
 
-// const CaseStudies = () => {
+const CaseStudies = () => {
   
-//   return (
-//     <>
-//       <div className="page" id="case-studies">
-//         <h1>My Projects</h1>
-//         <div className="projects">
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//           <h2>Bald Eagle Construction</h2>
-//           <div className="project">
-//             <div className="description">
-//               Describe client/project
-//             </div>
-//             <Image 
-//               src="/images/projects/bald-eagle.webp"
-//               alt="Screenshot of website built for Bald Eagle Construction"
-//               id = "bald-eagle"
-//               className='project-img'
-//               width={600}
-//               height={294}
-//               sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      {/* <div className="page" id="case-studies">
+        <h1>My Projects</h1>
+        <div className="projects">
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+          <h2>Bald Eagle Construction</h2>
+          <div className="project">
+            <div className="description">
+              Describe client/project
+            </div>
+            <Image 
+              src="/images/projects/bald-eagle.webp"
+              alt="Screenshot of website built for Bald Eagle Construction"
+              id = "bald-eagle"
+              className='project-img'
+              width={600}
+              height={294}
+              sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+            />
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+}
  
-// export default CaseStudies;
+export default CaseStudies;
