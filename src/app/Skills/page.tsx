a25ac5d13e62826c0ca60070dd0c282528f56ca1
Brian Quinney <quinneybrianm@gmail.com>
2024-08-26 14:38:38 -0400

    added basic styling for skills page

--- src/app/Skills/page.tsx
+++ src/app/Skills/page.tsx
@@ -2,24 +2,77 @@
 import React from "react"
 import './page.scss'
 import DownArrow from '../components/DownArrow/DownArrow'
+import { hd1080Min, hd4kMin, mobileMin, tabletMin } from "../_vars"
+import Image from "next/image"
 
 const Skills = () => {
 
     return (
       <>
         <div className="page" id="skills">
-          <h1>Welcome</h1>
-          <span className="inline-span">
-            <h3>I am&nbsp;</h3>
-            <h2>Brian Quinney</h2>
-          </span>
-          <span className="inline-span">
-            <h3>a&nbsp;</h3>
-            <h2>Full-Stack Web Engineer</h2>
-          </span><br />
-          <h3>Developing&nbsp;</h3>
-          <h2>Responsive, Dynamic, Elegant</h2>
-          <h3>Websites & Web Apps</h3>
+          <h1>Professional/Technical Skills</h1>
+          <div className="cards">
+            <div className="skill-wrapper">
+            <h3>HTML5</h3>
+              <div className="skill" id="html5">
+                  <Image
+                    src='/images/skills/HTML5.png'
+                    alt='html5'
+                    id='html5img'
+                    className='skill-img'
+                    width={1024}
+                    height={1024}
+                    sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+                  />
+              </div>
+            </div>
+
+
+            <div className="skill-wrapper">
+            <h3>HTML5</h3>
+              <div className="skill" id="html5">
+                  <Image
+                    src='/images/skills/HTML5.png'
+                    alt='html5'
+                    id='html5img'
+                    className='skill-img'
+                    width={1024}
+                    height={1024}
+                    sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+                  />
+              </div>
+            </div>
+            <div className="skill-wrapper">
+            <h3>HTML5</h3>
+              <div className="skill" id="html5">
+                  <Image
+                    src='/images/skills/HTML5.png'
+                    alt='html5'
+                    id='html5img'
+                    className='skill-img'
+                    width={1024}
+                    height={1024}
+                    sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+                  />
+              </div>
+            </div>
+            <div className="skill-wrapper">
+            <h3>HTML5</h3>
+              <div className="skill" id="html5">
+                  <Image
+                    src='/images/skills/HTML5.png'
+                    alt='html5'
+                    id='html5img'
+                    className='skill-img'
+                    width={1024}
+                    height={1024}
+                    sizes={`(min-width: ${hd4kMin}px) 61.8%, (min-width: ${hd1080Min}px)  61.8%, (min-width: ${tabletMin}px)  61.8%, (min-width: ${mobileMin}px)  61.8%,  61.8%`}
+                  />
+              </div>
+            </div>
+
+            
+          </div>
           <DownArrow nextPage="about" />
         </div>
       </>
